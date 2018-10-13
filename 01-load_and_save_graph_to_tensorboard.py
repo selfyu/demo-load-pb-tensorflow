3bf29db369acb2127fa656d0b6ecb68021dec546
selfyu <self.yu@gmail.com>
2018-10-13 18:04:47 +0800

    covert east
    Signed-off-by: selfyu <self.yu@gmail.com>

--- 01-load_and_save_graph_to_tensorboard.py
+++ 01-load_and_save_graph_to_tensorboard.py
@@ -6,7 +6,7 @@ from tensorflow.core.protobuf import saved_model_pb2
 from tensorflow.python.util import compat
 with tf.Session() as persisted_sess:
   print("load graph")
-  with gfile.FastGFile("./output_graph_510.pb",'rb') as f:
+  with gfile.FastGFile("./frozen_east_text_detection.pb",'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     persisted_sess.graph.as_default()
@@ -14,6 +14,7 @@ with tf.Session() as persisted_sess:
     writer = tf.summary.FileWriter("./tf_summary", graph=persisted_sess.graph)
     # Print all operation names
     for op in persisted_sess.graph.get_operations():
-      print(op)
+      #print(op)
+      pass
     # next: do the following in bash:
     # tensorboard --logdir ./tf_summary/
